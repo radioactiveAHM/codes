150287cb2cfff5802a621233f6494fe75cb65057
ahmadreza <djahmadfire@gmail.com>
2025-01-18 03:47:26 +0330

    generalic

--- rust.rs
+++ rust.rs
@@ -1,16 +1,5 @@
-/// Provides a &'static str from a str for sending &str to threads or tasks.
+/// Provides a &'static T from a T for sending a &'static T to threads or tasks.
 /// Cheap to clone and move around.
-/// Panics if the &str source lifetime is shorter than UnsafeStr.
-#[derive(Clone, Copy)]
-struct UnsafeStr{
-    value: &'static str
-}
-impl UnsafeStr {
-    fn new(string: &str) ->Self{
-        unsafe {
-            Self {
-                value: core::str::from_utf8_unchecked(std::slice::from_raw_parts(string.as_ptr(), string.len()))
-            }
-        }
-    }
-}
+/// Panics if the &T source lifetime is shorter than &'static T.
+
+pub fn unsafe_staticref<'a, T: ?Sized>(r: &'a T) -> &'static T { unsafe { std::mem::transmute::<&'a T, &'static T>(r) } }
